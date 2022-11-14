887c23be4cd09addbdf3f6c0fea8a132f5543201
AkhilaVulluri <akhivulluri19@gmail.com>
2022-11-14 18:01:47 +0530

    create new file odd and even.

--- odd_even.c
+++ odd_even.c
@@ -3,9 +3,10 @@ int main() {
     int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-
+//for even no.
     if(num % 2 == 0)
         printf("%d is even.", num);
+    //for odd no.
     else
         printf("%d is odd.", num);
     
